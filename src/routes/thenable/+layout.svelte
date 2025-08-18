e198b77f8089f290923c61c143c1c420cc12dc9d
Tom Ballinger <tom@convex.dev>
2025-08-18 13:24:44 -0700

    convert spaces to tabs

--- src/routes/thenable/+layout.svelte
+++ src/routes/thenable/+layout.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-    import type { LayoutProps } from './$types';
+	import type { LayoutProps } from './$types';
 
-    let { data, children }: LayoutProps = $props();
+	let { data, children }: LayoutProps = $props();
 </script>
 <svelte:boundary onerror={(e) => {
-    console.error(e)
+	console.error(e)
 }}>
-    {@render children()}
+	{@render children()}
 
-    {#snippet pending()}
-        <div>Loading...</div>
-    {/snippet}
-    {#snippet failed(error, reset)}
-        <p>Error: {error}</p>
+	{#snippet pending()}
+		<div>Loading...</div>
+	{/snippet}
+	{#snippet failed(error, reset)}
+		<p>Error: {error}</p>
 		<button onclick={reset}>oops! try again</button>
 	{/snippet}
 </svelte:boundary>
